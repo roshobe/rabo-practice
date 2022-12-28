1a9df92816c53742ca8cb53ec8a7317494c27134
roshobe <gigioshobe21@gmail.com>
2022-12-28 11:43:20 -0500

    made changes and added chart.js

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,14 +1,64 @@
 import React from "react";
 import DataCard from "./DataCard";
+import MyChart from "./MyChart";
 
 function Feed() {
+  const data = {
+    stockFullName: "SW Limited.",
+    stockShortName: "ASX:SFW",
+    price: {
+      current: 2.32,
+      open: 2.23,
+      low: 2.215,
+      high: 2.325,
+      cap: 93765011,
+      ratio: 20.1,
+      dividend: 1.67,
+    },
+    chartData: {
+      labels: [
+        "10:00",
+        "",
+        "",
+        "",
+        "12:00",
+        "",
+        "",
+        "",
+        "2:00",
+        "",
+        "",
+        "",
+        "4:00",
+      ],
+      data: [
+        2.23,
+        2.215,
+        2.22,
+        2.25,
+        2.245,
+        2.27,
+        2.28,
+        2.29,
+        2.3,
+        2.29,
+        2.325,
+        2.325,
+        2.32,
+      ],
+    },
+  };
+
   return (
-    <div className="bg-red-400 p-4">
+    <div className="bg-red-400 p-4 w-screen space-y-4">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
         <DataCard />
         <DataCard />
         <DataCard />
       </div>
+      <div className="p-4">
+          <MyChart info={data}/>
+        </div>
     </div>
   );
 }
